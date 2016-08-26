528cbd9c8b92f5c71df3e31565f9c5a9bbcfa25f
kyrychenkoV <keryvn@gmail.com>
2016-08-26 11:46:02 +0300

    array

--- main.c
+++ main.c
@@ -1,71 +1,66 @@
 #include <stdio.h>
+#include <stdbool.h>
+#define  MAXSIZE 30
 
-#define   N 1
-#define   M 0
-#define  S 40
-int validateJSON(char mass[]);
 
 int main(void)
 {
-    char mass [S]={"{{[]}}"};
+    char test[][MAXSIZE]={"][","[]","{}","{{[{{}{}},{},{[],[]}]}}","{[]}}","}{","{][}","[{]}"};
+    char (*prt)[MAXSIZE];
+    prt=test;
+    int sizeArray=sizeof(test)/sizeof(test[0]);
 
-    printf(validateJSON(mass)==N?"*TRUE*":"!FOLSE");
-
+    //printf (validateJSON(prt,sizeArray)==false? "FALSE":"TRUE");
+    validateJSON(prt,sizeArray);
 
     return 0;
 
 }
 
-int validateJSON(char mass[]){
-    int i;
-    char brace=M,square_bracket=M;
-    char o=M;
+void validateJSON(char prt[][MAXSIZE],int sizeArray){
+    int i,s;
+    int j;
+    int brace=NULL, square_bracket=NULL,o=NULL;
+        for (j=NULL;j<sizeArray;j++){
+            //int brace=NULL, square_bracket=NULL,o=NULL;
+            printf("%s\n",prt[j]);
+            for(i=NULL;i<strlen(prt[j]);i++){
+                if(prt[j][i]=='{'||prt[j][i]=='}'||prt[j][i]=='['||prt[j][i]==']'){
 
-            for(i=M;mass[i]!=M;i++){
-                if(mass[i]=='{'||mass[i]=='}'||mass[i]=='['||mass[i]==']'){
-
-                if(mass[i]=='}'){
+                if(prt[j][i]=='}'){
                     brace--;
 
                 }
-                if(brace<M){
+                if(brace<0){
                     o++;
                 }
-                if(mass[i]=='{'){
+                if(prt[j][i]=='{'){
                     brace++;
-
                 }
-
-
-                if(mass[i]==']'){
-                    --square_bracket;
-
+                if(prt[j][i]==']'){
+                    square_bracket--;
                 }
-                if(square_bracket<M){
-                    o++;
-
+                if(square_bracket<0){
+                    o--;
                 }
-
-                if(mass[i]=='['){
+                if(prt[j][i]=='['){
                     ++square_bracket;
+                }
+                }
 
-               }
-           }
-    }
+            }
+          //printf ("Brace=%d sqb=%d o=%d\n",brace,square_bracket,o);
+                if(brace==NULL&&square_bracket==NULL&&o==NULL){
+                    printf("TRUE\n");
+                }
+                else{
+                   printf("FALSE\n");
+                }
+            printf("\n");
+            brace=NULL, square_bracket=NULL,o=NULL;
 
-
-    printf ("Brace=%d sqb=%d o=%d\n",brace,square_bracket,o);
-
-        if(brace==M&&square_bracket==M&&o==M){
-
-            return N;
         }
-        else{
-
-            return M;
-        }
-
-
 }
+
 
 
